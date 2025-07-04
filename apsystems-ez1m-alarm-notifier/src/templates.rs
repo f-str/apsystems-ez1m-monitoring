39ef64e741cdceb86a8214ca3edd0b016ce567a5
Florian Straubinger <git@f-str.de>
2025-07-04 14:59:00 +0200

    Removes some not needed services

--- apsystems-ez1m-alarm-notifier/src/templates.rs
+++
@@ -1,8 +0,0 @@
-// TODO -> Write the templates for the alarms
-pub const OFFGRID_ALARM_TEMPLATE: &str = r#"<html></html>"#;
-
-pub const OUTPUT_FAULT_ALARM_TEMPLATE: &str = r#"<html></html>"#;
-
-pub const DC1_SHORT_CIRCUIT_ALARM_TEMPLATE: &str = r#"<html></html>"#;
-
-pub const DC2_SHORT_CIRCUIT_ALARM_TEMPLATE: &str = r#"<html></html>"#;
